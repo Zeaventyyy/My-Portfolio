13a3ad513b9917cd6c7f42e88ac1c898fe570af6
Zeaven Manalastas <zeavenjoverttemanalastas@gmail.com>
2022-02-27 18:55:00 +0800

    Added about

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css';
 import React from 'react';
 import Navbar from './landingpage/navbar';
 import Main from './landingpage/main';
+import About from './landingpage/about';
 
 function App() {
   return (
-    <div className="App flex flex-col background h-screen">
-      <Navbar />
-      <Main />
+    <div className="App ">
+      <div className="landingPage flex flex-col background h-screen">
+        <Navbar />
+        <Main />
+      </div>
+      <div className="about backgroundAbout h-screen">
+        <About />
+      </div>
     </div>
   );
 }
