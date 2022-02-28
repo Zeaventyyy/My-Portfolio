f5e95cc43a3953847115c506e8344f4490c54686
Zeaven Manalastas <zeavenjoverttemanalastas@gmail.com>
2022-02-28 21:07:50 +0800

    Fixed about component

--- src/landingpage/about.jsx
+++ src/landingpage/about.jsx
@@ -6,16 +6,21 @@ function About() {
     <div className="h-full container mx-auto">
       <div className="flex flex-col sm:flex-row justify-center items-center h-full px-10 lg:w-[52rem] mx-auto ">
         <section className="flex flex-col items-center text-white text-justify">
-          <h1 className="text-sm sm:text-base pb-3 font-medium ">
+          <div className="w-full flex justify-start mb-5">
+            <span className="text-2xl mr-10 font-medium ">About Me</span>
+            <span className="flex-grow border-[1px] opacity-50 self-center" />
+          </div>
+
+          <p className="text-sm sm:text-base pb-3 font-medium ">
             Hello! I&apos;m Zeaven Jovertte Manalastas and I enjoy building
             things that live on the internet. My web development journey started
             way back 2019 when I was an intern. I stopped for the meantime and
             become an android developer for a year and a 5 months.
-          </h1>
-          <h1 className="text-sm sm:text-base font-medium">
+          </p>
+          <p className="text-sm sm:text-base font-medium">
             Fast-forward to today, I&apos;ve been studying front end frameworks
-            and libraries to use it on my future projects and works.
-          </h1>
+            and libraries to use them on my future projects and works.
+          </p>
         </section>
         <section className="sm:flex self-end sm:items-center -mr-2 mt-4 text-white sm:h-full fill-white">
           <ZeavenLogo
