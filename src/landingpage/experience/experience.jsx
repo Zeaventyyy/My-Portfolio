820db385e5b8fb5109f5bc8b5a7f9b31fff061e2
Zeaven Manalastas <zeavenjoverttemanalastas@gmail.com>
2022-03-02 21:44:03 +0800

    Fixed font experience

--- src/landingpage/experience/experience.jsx
+++ src/landingpage/experience/experience.jsx
@@ -9,7 +9,9 @@ function Experience() {
       <div className="flex flex-col sm:flex-row justify-center items-center h-full px-10 lg:w-[52rem] mx-auto ">
         <section className="flex flex-col w-full text-white justify-center md:justify-start md:h-60">
           <div className="flex justify-start mb-5 w-full">
-            <p className="text-2xl mr-10 font-medium ">Where I’ve Worked</p>
+            <p className="text-lg md:text-2xl mr-10 font-semibold ">
+              Where I’ve Worked
+            </p>
             <p className="flex-grow border-[1px] opacity-50 self-center" />
           </div>
           <div className="flex flex-col items-center md:flex-row md:items-start w-full  ">
