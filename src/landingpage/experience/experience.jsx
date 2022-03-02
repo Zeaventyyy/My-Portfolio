950dfd138d015aafa73294b5dee5f05064a4db47
Zeaven Manalastas <zeavenjoverttemanalastas@gmail.com>
2022-03-02 20:31:36 +0800

    Added experience css and fixed content

--- src/landingpage/experience/experience.jsx
+++ src/landingpage/experience/experience.jsx
@@ -1,90 +1,42 @@
 import React, { useState } from 'react';
+import styles from './experience.module.css';
 
 function Experience() {
-  const [setActive, setActiveCmai] = useState(true);
-  const [setActiveOrangeApps, setActiveOrange] = useState(false);
-
-  const showCmai = () => {
-    setActiveCmai(true);
-    setActiveOrange(false);
-  };
-  const showOrangeApps = () => {
-    setActiveCmai(false);
-    setActiveOrange(true);
-  };
+  const [experience, setExperience] = useState('cmai');
 
   return (
     <div className="h-full container mx-auto">
       <div className="flex flex-col sm:flex-row justify-center items-center h-full px-10 lg:w-[52rem] mx-auto ">
-        <section className="flex flex-col w-full justify-center items-center text-white text-justify">
+        <section className="flex flex-col w-full text-white h-60">
           <div className="flex justify-start mb-5 w-full">
             <p className="text-2xl mr-10 font-medium ">Where I’ve Worked</p>
             <p className="flex-grow border-[1px] opacity-50 self-center" />
           </div>
-          <div className="flex flex-col items-center md:flex-row md:items-start w-full">
+          <div className="flex flex-col items-center md:flex-row md:items-start w-full  ">
             <div className="flex w-full flex-row md:flex-col items-start md:pl-6 md:mb-0 md:w-[17.25rem] mb-4">
               <button
-                className="hover:bg-black/25
-                hover:border-white
-                border-b-2
-                md:border-b-0
-                md:border-l-2 border-white/50
-              active:transition
-              active:duration-500
-              transition
-              duration-500
-              focus:border-b-2
-              focus:border-white
-              focus:bg-black/50
-              md:focus:border-l-2
-              md:focus:border-b-0
-              md:focus:border-white
-              px-8
-              py-2
-              w-full
-              h-full
-              text-center
-              md:text-left
-              "
+                className={`${experience === 'cmai' ? styles.active : ''} ${
+                  styles.btn
+                }`}
                 type="button"
-                onClick={showCmai}
+                onClick={() => setExperience('cmai')}
               >
                 CMAI
               </button>
               <button
-                className="hover:bg-black/25
-                hover:border-white
-                border-b-2
-                md:border-b-0
-                md:border-l-2 border-white/50
-                focus:border-b-2
-                focus:border-white
-              active:transition
-              active:duration-500
-              focus:bg-black/50
-              md:focus:border-l-2
-              md:focus:border-white
-              md:focus:border-b-0
-              focus:md:
-              transition
-              duration-500
-              px-8
-              py-2
-              w-full
-              h-full
-              text-center
-              md:text-left
-              "
+                className={`${
+                  experience === 'orangeApps' ? styles.active : ''
+                } ${styles.btn}`}
                 type="button"
-                onClick={showOrangeApps}
+                onClick={() => setExperience('orangeApps')}
               >
                 Orange Apps
               </button>
               <p className="hidden md:block border-[1px] opacity-50 self-end w-1/2 mt-8  " />
             </div>
 
-            {setActive ? (
-              <div className=" md:pl-10 grow">
+            {experience === 'cmai' ? (
+              <div className=" md:pl-10 grow animate-fade-in-down">
                 <p className="text-xl font-semibold mb-1">
                   Associate Android Developer
                 </p>
@@ -104,8 +56,8 @@ function Experience() {
                 </div>
               </div>
             ) : null}
-            {setActiveOrangeApps ? (
-              <div className="md:pl-10 grow">
+            {experience === 'orangeApps' ? (
+              <div className="md:pl-10 grow animate-fade-in-down">
                 <p className="text-xl font-semibold mb-1">
                   Frontend Developer/Quality Assurance Intern
                 </p>
