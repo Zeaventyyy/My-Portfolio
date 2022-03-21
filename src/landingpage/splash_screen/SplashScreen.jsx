59113c0d8bc6e2d7674fc9701edddae347b134c6
Zeaven Manalastas <zeavenjoverttemanalastas@gmail.com>
2022-03-21 11:46:58 +0800

    Fixed animation for splash screen

--- src/landingpage/splash_screen/SplashScreen.jsx
+++ src/landingpage/splash_screen/SplashScreen.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { ReactComponent as ZeavenLogo } from '../../icons/zeavenlogo.svg';
+import styles from './splash_screen.module.css';
 
 function SplashScreen() {
   const [isVisible, setVisibility] = useState(true);
 
-  setTimeout(() => setVisibility(false), 3000);
+  setTimeout(() => setVisibility(false), 4000);
 
   return (
     isVisible && (
-      <div className="splash-screen bg-amber-500 h-screen flex items-center justify-center fixed top-0 right-0 left-0 bottom-0 z-10 ">
-        <ZeavenLogo className="animate-spin" />
-        <ZeavenLogo className="animate-spin" />
+      <div className={styles.splashScreen}>
+        <ZeavenLogo className="animate-ping-slow" />
       </div>
     )
   );
